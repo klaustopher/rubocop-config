05f1853c3e21104cb1c8601f93a9f5f67c6a2257
Klaus Zanders <code@kgz.me>
2023-07-28 13:36:23 +0200

    Add a sample rails application

--- sample/spec/models/user_spec.rb
+++
@@ -1,9 +0,0 @@
-require "rails_helper"
-
-RSpec.describe User do
-  subject { FactoryBot.create(:user) }
-
-  it "does things" do
-    expect(described_class.new).to be_a(described_class)
-  end
-end
